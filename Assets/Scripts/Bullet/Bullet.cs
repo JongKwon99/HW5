1fd59ee61254a078d95aaacb5941c6ad22b2d95a
unknown <john9912@naver.com>
2024-12-16 01:37:01 +0900

    파티클 일부 적용

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed;
+    public GameObject effect;
 
     private void Update()
     {
@@ -14,6 +15,7 @@ public class Bullet : MonoBehaviour
     // 무언가와 부딪히면 나 자신을 파괴
     private void OnTriggerEnter(Collider other)
     {
+        Instantiate(effect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
